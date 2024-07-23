d360ab0872bff58722e17a0a7597a9dd6438d94d
lovehachiware <dante3518@naver.com>
2024-07-23 22:23:49 +0900

    병함됨?

--- dhsproject/blog/views.py
+++ dhsproject/blog/views.py
@@ -54,3 +54,45 @@ class CommentListView(generics.ListAPIView):
         post_id = self.kwargs['post_id']
         post = get_object_or_404(Post, id=post_id, user=self.request.user)
         return Comment.objects.filter(post=post).order_by('created_at')
+
+### 메인페이지(date기준 오름차순 사용자의 모든 게시물 조회) + 최근 10개의 게시물 기준 평균 달성률 추가
+class UserPostListView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        posts = Post.objects.filter(user=request.user).order_by('-date')
+        serializer = PostListSerializer(posts, many=True)
+
+        # 최근 10개의 게시물 기준 평균 달성률 계산
+        recent_posts = posts[:10]  # 최근 10개의 게시물을 가져옴
+
+        total_achievement_rate = 0
+        for post in recent_posts:
+            achievement_rate = post.achievement_rate()
+            total_achievement_rate += achievement_rate
+
+        count = recent_posts.count()
+        average_achievement_rate = round(total_achievement_rate / count, 2) if count > 0 else 0
+
+        return Response({
+            'posts': serializer.data,
+            'TenDaysAverage': average_achievement_rate
+        }, status=status.HTTP_200_OK)
+    
+###한페이지뷰(사용자의 특정 한 페이지 조회 (게시물 id 이용))
+class PostDetailView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id, user=request.user)
+        serializer = PostDetailSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+###게시물삭제
+class PostDeleteView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id, user=request.user)
+        post.delete()
+        return Response({"message": "게시물이 성공적으로 삭제되었습니다."}, status=status.HTTP_204_NO_CONTENT)
