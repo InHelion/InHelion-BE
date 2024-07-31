69d7b16348ffbb2b4e57c5a642fd4a0e58b919ef
gayo73 <163620291+gayo73@users.noreply.github.com>
2024-07-31 17:16:14 +0900

    최종

--- dhsproject/config/settings.py
+++ dhsproject/config/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     'users',
     'blog',
     'rest_framework',
-    'rest_framework_simplejwt.token_blacklist',
+    #'rest_framework_simplejwt.token_blacklist',
+    'rest_framework_simplejwt',
     #swagger
     'drf_yasg'
 ]
@@ -173,6 +174,12 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',  # 인증된 요청인지 확인
+        #'rest_framework.permissions.AllowAny',  # 누구나 접근 가능 
+				# (기본적으로 누구나 접근 가능하게 설정하고, 인증된 요청인지 확인하는 api를 따로 지정하게 하려면 
+				# 이 옵션을 위의 옵션 대신 켜주어도 됩니다!)
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
@@ -181,10 +188,13 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'users.CustomUser'
 
 SIMPLE_JWT = {
+    'SIGNING_KEY': 'dahaeshoodahaeshoo',
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=7),  # Access 토큰의 유효 기간
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'BLACKLIST_AFTER_ROTATION': True
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
 }
+REST_USE_JWT = True
 
 
 # 이메일 설정
