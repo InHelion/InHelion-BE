c6101cfa78f7a95c17c5283450504c4aeacf8a6c
gayo73 <163620291+gayo73@users.noreply.github.com>
2024-08-03 22:38:54 +0900

    이메일 수정

--- dhsproject/blog/urls.py
+++ dhsproject/blog/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('posts/<int:post_id>/comments/', CommentListView.as_view(), name='comment-list'), # 페이지 1개의 모든 댓글 조회
     path('posts/<int:post_id>/comments/create/', CommentCreateView.as_view(), name='comment-create'), # 댓글 생성
     path('posts/<int:post_id>/comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'), # 댓글 삭제
-    path('miss_email/<int:user_id>/', MissEmailNotificationView.as_view(), name='miss_email_notification') # 보고싶어 이메일
+    path('miss_email/', MissEmailNotificationView.as_view(), name='miss_email_notification') # 보고싶어 이메일
 
     #path('', PostListView.as_view(), name='post-list'),
     #path('<int:pk>/comments/update/', CommentUpdateView.as_view(), name='comment-update'), # 댓글 수정
